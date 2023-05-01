9ac7ccad8b4b5feb6bd427d935a012c8ebf80594
Graham Daw <grahamdaw@users.noreply.github.com>
2023-05-01 23:21:25 +0300

    Adding layout for basic page

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer>
+    <p>Template source @ <a href="https://github.com/grahamdaw/my-svelte-template">grahamdaw/my-svelte-template</a></p>
+</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="app bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Template source @ <a href="https://github.com/grahamdaw/my-svelte-template">grahamdaw/my-svelte-template</a></p>
+	</footer>
+</div>
+
+<style lang="postcss">
+    .app {
+        min-height: 100vh;
+    }
+</style>
